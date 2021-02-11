6aa4f7db41744dffcb3880b2740cb47138a59537
Jonathan King <jonpking@gmail.com>
2021-02-10 20:52:44 -0700

    further refactor/development of api call to unsplash, created seperate file for some configuration of api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
-
-const API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        axios.get("https://api.unsplash.com/search/photos", {
+    state = {
+        images: []
+    };
+
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get("/search/photos", {
             params: { query: term },
-            headers: {
-                Authorization: `Client-ID ${API_KEY}`
-            }
         });
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: "10px" }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
